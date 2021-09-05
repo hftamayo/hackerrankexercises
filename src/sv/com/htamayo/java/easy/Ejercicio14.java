d310cfc6863cc48c0d76fca4d6691b463e0b14ab
hftamayo <hftamayo@gmail.com>
2021-09-05 17:05:32 -0600

    exercises easy

--- src/sv/com/htamayo/java/easy/Ejercicio14.java
+++ src/sv/com/htamayo/java/easy/Ejercicio14.java
@@ -8,7 +8,9 @@ import java.util.regex.*;
 
 public class Ejercicio14 {
     public static void main(String[] args){
-        ArrayList dataList = new ArrayList();
+        List<Integer> lstValues = new ArrayList<>();
+        Map<Integer, List<Integer> > dataMap = new HashMap<>();
+        Map<Integer, Integer> coordsMap = new HashMap<Integer, Integer>();
         int lineQuantity = 0;
         int itemNumbers = 0;
         int itemValue = 0;
@@ -16,25 +18,47 @@ public class Ejercicio14 {
         int y = 0;
 
         Scanner rawData = new Scanner(System.in);
+        System.out.println("limit of lines:");
         lineQuantity = rawData.nextInt();
         //data fill
-        for(int i=0; i<lineQuantity; i++){
+        for(int i=1; i<=lineQuantity; i++){
+            System.out.println("limit of elements for line:" + i);
             itemNumbers = rawData.nextInt();
-            dataList.add(itemNumbers);
-            for(int j=0; j<itemNumbers; j++){
+            for(int j=1; j<=itemNumbers; j++){
+                System.out.println("element of line: "+j);
                 itemValue = rawData.nextInt();
-                dataList.add(itemValue);
+                lstValues.add(itemValue);
             }
+            dataMap.put(i, lstValues);
+            //lstValues.clear();
         }//fin del for
 
+        System.out.println("How many coords line:");
         lineQuantity = rawData.nextInt();
-        x = rawData.nextInt();
-        y = rawData.nextInt();
+        for(int i=1; i<=lineQuantity; i++){
+            System.out.println("enter X, Y coords: ");
+            x = rawData.nextInt();
+            y = rawData.nextInt();
+            coordsMap.put(x, y);
+        }
 
         //iterate datalist content
+        System.out.println("arraylist content:");
+        for(Map.Entry<Integer, List<Integer>> data: dataMap.entrySet()){
+            System.out.println("key="+data.getKey()+ ", value="+data.getValue());
+        }
+
+        /*
+
         for(int i = 0; i<dataList.size(); i++){
             System.out.println(dataList.get(i));
+        }
 
+         */
+
+        System.out.println("coords needed");
+        for(Map.Entry<Integer, Integer> coords: coordsMap.entrySet()){
+            System.out.println("key="+coords.getKey()+ ", value="+coords.getValue());
         }
     }
 }
