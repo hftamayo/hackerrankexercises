9a3049166da2e44ed45af43fa2cee1e12598d5e2
hftamayo <hftamayo@gmail.com>
2022-03-07 20:47:53 -0600

    exercise 26 part1

---
+++ src/sv/com/htamayo/java/challenge30daysept/Day26NestedIf.java
@@ -0,0 +1,20 @@
+package sv.com.htamayo.java.challenge30daysept;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+        int month = sc.nextInt();
+        int year = sc.nextInt();
+        int dayExpected = sc.nextInt();
+        int monthExpected = sc.nextInt();
+        int yearExpected = sc.nextInt();
+        int fine = 0;
+    }
+}
+
