526fdb87cad691ad3ea86e588eb61c7365e7d0b5
hftamayo <hftamayo@gmail.com>
2022-03-08 19:24:02 -0600

    exercise 26 part 2

--- src/sv/com/htamayo/java/challenge30daysept/Day26NestedIf.java
+++ src/sv/com/htamayo/java/challenge30daysept/Day26NestedIf.java
@@ -15,6 +15,37 @@ public class Solution {
         int monthExpected = sc.nextInt();
         int yearExpected = sc.nextInt();
         int fine = 0;
+        
+        /*
+        el siguiente codigo paso 8/10 casos
+        
+        if((yearExpected - year) < 0){
+            fine = 10000;
+        } else if((monthExpected - month) < 0) {
+            fine = 500 * (month - monthExpected);
+        } else if((dayExpected - day) < 0){
+            fine = 15 * (day - dayExpected);
+        }
+        
+        System.out.println(fine);
+        
+        */
+        
+        /*
+        el siguiente codigo paso todos los tests, la diferencia es que no evalue explicitamente 
+        si el mes o año recibida fue exactamente igual al esperado 
+        */
+        
+        if(year > yearExpected){
+            fine = 10000;
+        }else if(year == yearExpected && month > monthExpected){
+            fine = (month - monthExpected) * 500;
+        }else if(year == yearExpected && month == monthExpected && day > dayExpected){
+            fine = (day - dayExpected) * 15;
+        }
+        
+        System.out.println(fine);                
+        
     }
 }
 
